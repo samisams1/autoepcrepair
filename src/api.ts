a7f9a7eab47448985f1b35b426690e6db4a598bd
samisasm1 <forsamisams@gmail.com>
2024-04-23 11:38:01 +0300

    update pages

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
  // baseURL: 'http://localhost:4000',
-  baseURL:'http://test2.nilesoftdemo.com'
+  baseURL:'https://test2.nilesoftdemo.com'
 });
 
 export default api;
